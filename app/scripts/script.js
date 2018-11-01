0d55ee9940e457c6138d41e576fe78105b587704
Wonmi <wonmadesign@gmail.com>
2018-11-02 03:59:51 +0900

    Built to-do list machine (second time with cleaner code)

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -2,6 +2,13 @@ var input = document.getElementById("todoBox__input")
 var inputBtn = document.getElementById("todoBox__btn")
 var listBox = document.getElementById("todoBox__list")
 
+listBox.addEventListener('click', event => {
+    if(event.target.localName === "li") {
+        event.target.classList.toggle('is-clicked')
+    } else if(event.target.localName === "button") {
+        event.target.parentNode.remove()
+    }
+})
 
 function createListItem() {
         var newItem = document.createElement("li")
@@ -20,24 +27,22 @@ function createListItem() {
         input.value = ""
 }
 
+
 function isInputValid() {
     return input.value.length
 }
 
 function createAfterClick() {
-    if (isInputValid > 0) {
+    if (isInputValid() > 0) {
         createListItem()
     }
 }
 
 function createAfterEnter(e) {
-    if (e.which === 13 && isInputValid > 0) {
+    if (e.which === 13 && isInputValid() > 0) {
         createListItem()
     }
 }
 
 inputBtn.addEventListener("click", createAfterClick)
 input.addEventListener("keypress", createAfterEnter)
-
-// 저지른 오류 : input.value은 노드 타입이 아니라고 나옴.
-// span자식으로 가능한 type은 node타입이라서 그랬구나! 
